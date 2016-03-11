ab1d3eb5010ee97a3f7f6af50edb94b16886745e
j10sanders <jps458@nyu.edu>
2016-03-11 01:42:58 +0000

    started db and removed vulnerability

---
+++ fullstack/vagrant/forum/forumdb.py
@@ -0,0 +1,42 @@
+#
+# Database access functions for the web forum.
+# 
+
+import time, psycopg2
+
+## Database connection
+#DB = []
+
+## Get posts from database.
+def GetAllPosts():
+    DB = psycopg2.connect("dbname=forum")
+
+    '''Get all the posts from the database, sorted with the newest first.
+
+    Returns:
+      A list of dictionaries, where each dictionary has a 'content' key
+      pointing to the post content, and 'time' key pointing to the time
+      it was posted.
+    '''
+    #posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
+    c = DB.cursor()
+    c.execute("SELECT time, content from posts ORDER BY time DESC");
+    posts = ({'content': str(row[1]), 'time': str(row[0])}
+              for row in c.fetchall())
+    DB.close()
+    #posts.sort(key=lambda row: row['time'], reverse=True)
+    return posts
+
+## Add a post to the database.
+def AddPost(content):
+    DB = psycopg2.connect("dbname=forum")
+    '''Add a new post to the database.
+
+    Args:
+      content: The text content of the new post.
+    '''
+    c = DB.cursor()
+    c.execute("INSERT INTO posts (content) VALUES (%s)", (content,))
+    DB.commit()
+    DB.close()
+    #DB.append((t, content))
